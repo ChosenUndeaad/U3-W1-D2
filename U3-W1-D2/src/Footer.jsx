83a2e44894e84213e443d6f1a928339ab2c65fe2
ChosenUndeaad <32023627+ChosenUndeaad@users.noreply.github.com>
2025-04-01 16:12:40 +0200

    Update

---
+++ U3-W1-D2/src/Footer.jsx
@@ -0,0 +1,11 @@
+function MyFooter() {
+  return (
+    <>
+      <div className='fixed-bottom bg-dark text-white text-center py-3'>
+        <p>Diritti riservati - 205</p>
+      </div>
+    </>
+  );
+}
+
+export default MyFooter;
