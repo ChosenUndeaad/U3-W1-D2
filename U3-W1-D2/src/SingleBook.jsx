7fdae178d5ce6a094f6b663531bbb71e7c7b9fe7
ChosenUndeaad <32023627+ChosenUndeaad@users.noreply.github.com>
2025-04-02 15:40:14 +0200

    Update SingleBook.jsx

--- U3-W1-D2/src/SingleBook.jsx
+++ U3-W1-D2/src/SingleBook.jsx
@@ -4,34 +4,19 @@ import { Container, Row, Col } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import Card from "react-bootstrap/Card";
 
-class SingleBook extends Component {
-  state = {
-    selected: false,
-  };
-
-  render() {
-    const { book } = this.props;
-
-    return (
-      <Card
-        className='p-4 bg-dark text-light h-100 d-flex flex-column'
-        style={{ width: "18rem", margin: "10px" }}
-      >
-        <Card.Img
-          variant='top'
-          src={book.img}
-          alt={book.title}
-          style={{ objectFit: "cover", height: "250px" }}
-        />
-        <Card.Body className='d-flex flex-column flex-grow-1'>
-          <Card.Title>{book.title}</Card.Title>
-          <Button variant='primary' className='mt-auto'>
-            More Info
-          </Button>
-        </Card.Body>
-      </Card>
-    );
-  }
-}
+const SingleBook = ({ book }) => {
+  return (
+    <Card
+      style={{ width: "18rem", margin: "10px" }}
+      className='p-4 bg-dark text-light h-100'
+    >
+      <Card.Img variant='top' src={book.img} alt={book.title} />
+      <Card.Body>
+        <Card.Title>{book.title}</Card.Title>
+        <Button variant='primary'>More Info</Button>
+      </Card.Body>
+    </Card>
+  );
+};
 
 export default SingleBook;
